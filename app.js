057e254dfdccf1bd9c26dacdcbb2a61f937164ed
WAREWO <storm.riyu@gmail.com>
2026-08-28 19:09:02 +0700

    template dashboard iot especially for humidity and temperature

---
+++ app.js
@@ -0,0 +1,62 @@
+// variable untuk menyimpan alamat broker hivemq
+const brokerUrl = 'wss://broker.hivemq.com:8884/mqtt';
+
+// membuat client id
+const options = {
+    clientId: 'webDashboard_'+ Math.random().toString(16).substring(2,8)
+};
+
+// menyimpan id status agar bisa merubah online menjadi connecting....
+const status_server = document.getElementById('status_server');
+
+// merubah online menjadi connecting.....
+status_server.innerText = "Connecting.....";
+
+// menghubungkan dengan mqtt
+const client = mqtt.connect(brokerUrl,options);
+
+// function ketika mqtt sudah berhasil terkoneksikan
+client.on('connect',function(){
+    console.log("Berhasil terhubung dengan mqtt");
+    status_server.innerText = "Online";
+    status_server.style.color = "#2ecc71"; //mengganti warna status server
+    // subscribe ke mqtt 
+    client.subscribe('iot/esp32/dht11/suhu',function(error){
+        if(!error){
+            console.log("MQTT berhasil terkoneksikan dengan suhu!!!!");
+        }
+        else{
+            console.log("Tidak bisa terkoneksi karena :" + error);
+        }
+    });
+    client.subscribe('iot/esp32/dht11/hum',function(error){
+        if(!error){
+            console.log("MQTT berhasil terkoneksikan dengan kelembapan!!!!");
+        }
+        else{
+            console.log("Tidak bisa terkoneksi karena :" + error);
+        }
+    });
+});
+
+// mendengarkan pesan masuk: ini jalan ketika ada data masuk ke topik yang kita subscribe
+client.on('message',function(topik,message){
+    // merubah karena massage itu byte kita ubah jadi string
+    var pesanMasuk = message.toString();
+    console.log("Pesan diterima dari topik " + topik + ":" + pesanMasuk);
+
+    // merubah tampilan html berdasarkan pesan yang masuk dari mqtt
+    var elementSuhu = document.getElementById("suhu");
+    var elementKelembapan = document.getElementById("kelembapan");
+    if (topik === 'iot/esp32/dht11/suhu') {
+        elementSuhu.innerText = pesanMasuk;
+    }
+    else if(topik === 'iot/esp32/dht11/hum'){
+        elementKelembapan.innerText = pesanMasuk;
+    }
+});
+
+var indikasiSuhu = document.getElementById('suhu');
+
+
+
